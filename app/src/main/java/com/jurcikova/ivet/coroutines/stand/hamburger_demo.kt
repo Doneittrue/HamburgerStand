cf94e16718ef4ed674ce419be4410c8dffca7522
ivet <iveta.jurcikova@strv.com>
2019-03-07 11:35:00 +0100

    Create high season hamburger stand with channels

--- app/src/main/java/com/jurcikova/ivet/coroutines/stand/hamburger_demo.kt
+++ app/src/main/java/com/jurcikova/ivet/coroutines/stand/hamburger_demo.kt
@@ -2,13 +2,26 @@ package com.jurcikova.ivet.coroutines.stand
 
 import com.jurcikova.ivet.coroutines.stand.entity.*
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
 import kotlin.system.measureTimeMillis
 
+@ObsoleteCoroutinesApi
+@ExperimentalCoroutinesApi
 fun main() {
     val time = measureTimeMillis {
         runBlocking {
+            val ordersChannel =
+                produce(CoroutineName("Lenka")) {
+                    orders(3).forEach { order ->
+                        send(order)
+                    }
+                }
+
             launch(CoroutineName("Ivet")) {
-                makeHamburger(orders())
+                makeHamburger(ordersChannel)
+            }
+            launch(CoroutineName("Dominika")) {
+                makeHamburger(ordersChannel)
             }
         }
     }
@@ -16,47 +29,49 @@ fun main() {
     log("Order processed in $time milliseconds")
 }
 
-private fun makeHamburger(orders: List<Order>) {
-    runBlocking {
-        orders.forEach { order ->
-            log("Processing ${order.id}. order")
+@ObsoleteCoroutinesApi
+private suspend fun makeHamburger(
+    ordersChannel: ReceiveChannel<Order>
+) = coroutineScope {
+    ordersChannel.consumeEach { order ->
+        log("Processing ${order.id}. order")
 
-            val vegetable = cutVegetable(order.id)
-            val meat = async { fryMeat(order.id) }
-            val bun = async { heatBun(order.id) }
-            val hamburger = prepareHamburger(vegetable, meat.await(), bun.await())
+        val vegetable = cutVegetable(order.id)
+        val meat = async { fryMeat(order.id) }
+        val bun = async { heatBun(order.id) }
+        val hamburger =
+            prepareHamburger(
+                vegetable,
+                meat.await(),
+                bun.await()
+            )
 
-            log("Serve $hamburger")
-        }
+        log("Serve $hamburger")
     }
 }
 
 //business methods
 private suspend fun cutVegetable(orderId: Int): Vegetable {
-    log("Start cutting vegetable")
+    log("Cutting vegetable")
     delay(1000)
-    log("Stop cutting vegetable")
     return Vegetable(orderId)
 }
 
 private suspend fun fryMeat(orderId: Int): Meat {
-    log("Start frying meat")
+    log("Frying meat")
     delay(2000)
-    log("Stop frying meat")
     return Meat(orderId)
 }
 
 private suspend fun heatBun(orderId: Int): Bun {
-    log("Start heating bun")
+    log("Heating bun")
     delay(500)
-    log("Stop heating bun")
     return Bun(orderId)
 }
 
 private suspend fun prepareHamburger(vegetable: Vegetable, meat: Meat, bun: Bun): Hamburger {
-    log("Start preparing hamburger")
+    log("Preparing hamburger")
     delay(500)
-    log("Stop preparing hamburger")
     return Hamburger(vegetable, meat, bun)
 }
 
@@ -69,3 +84,4 @@ fun orders(quantity: Int = 1) =
 fun log(message: String) {
     println("[${Thread.currentThread().name}] $message")
 }
+
